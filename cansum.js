80ae273adc08eebc7a83b93439b33b0b42dcdbd2
Neil Feris <neil.feris@gmail.com>
2022-05-25 06:39:09 +0200

    fixes python version

--- cansum.js
+++ cansum.js
@@ -1,15 +1,22 @@
-const canSum = (targetSum, numbers) => {
+const canSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
   if (targetSum === 0) return true;
   if (targetSum < Math.min(...numbers)) return false;
 
   for (let num of numbers) {
     remainder = targetSum - num;
-    if (canSum(remainder, numbers)) return true;
+    if (canSum(remainder, numbers, memo)) {
+      memo[targetSum] = true;
+      return true;
+    }
   }
+  memo[targetSum] = false;
   return false;
 };
 
-console.log(canSum(7, [2, 4]));
-console.log(canSum(7, [3, 5, 4, 7]));
-// console.log(canSum(7, [2, 4]));
-// console.log(canSum(7, [2, 4]));
+console.log(canSum(7, [2, 4])); // false
+console.log(canSum(7, [3, 5, 4, 7])); // true
+console.log(canSum(15, [2, 4, 8, 10])); // false
+console.log(canSum(15, [3, 5, 4, 7])); // true
+console.log(canSum(210, [7, 14])); // true
+console.log(canSum(300, [7, 14])); // false
--- cansum.py
+++ cansum.py
@@ -1,4 +1,6 @@
-def cansum(targetSum, numbers, memo={}):
+def cansum(targetSum, numbers, memo=None):  # avoid default dict argument
+    if memo is None:
+        memo = {}
     if targetSum in memo:
         return memo[targetSum]
     if targetSum == 0:
@@ -16,8 +18,14 @@ def cansum(targetSum, numbers, memo={}):
 
 
 print(cansum(7, [2, 4]))
+# false
 print(cansum(7, [3, 5, 4, 7]))
+# true
 print(cansum(15, [2, 4, 8, 10]))
+# false
 print(cansum(15, [3, 5, 4, 7]))
-# print(cansum(200, [7, 14]))
-# print(cansum(300, [7, 14]))
+# true
+print(cansum(210, [7, 14]))
+# true
+print(cansum(300, [7, 14]))
+# false
